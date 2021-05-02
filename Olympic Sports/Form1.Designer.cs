c37af1e8db51a786251aba1a69aebca23f497857
Yerkezhan Galymbekova <72787327+Kesha201@users.noreply.github.com>
2021-05-03 02:03:21 +0600

    commit changes

---
+++ Olympic Sports/Form1.Designer.cs
@@ -0,0 +1,327 @@
+﻿
+namespace Olimpic_Sport
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataSet3 = new Olimpic_Sport.DataSet3();
+            this.pOPULATIONBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.pOPULATIONTableAdapter = new Olimpic_Sport.DataSet3TableAdapters.POPULATIONTableAdapter();
+            this.tableAdapterManager = new Olimpic_Sport.DataSet3TableAdapters.TableAdapterManager();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tb_country = new System.Windows.Forms.TextBox();
+            this.tb_code = new System.Windows.Forms.TextBox();
+            this.tb_population = new System.Windows.Forms.TextBox();
+            this.tb_gdp = new System.Windows.Forms.TextBox();
+            this.gb_population = new System.Windows.Forms.GroupBox();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.btn_update = new System.Windows.Forms.Button();
+            this.btn_delete = new System.Windows.Forms.Button();
+            this.btn_reset = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pOPULATIONBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.gb_population.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataSet3
+            // 
+            this.dataSet3.DataSetName = "DataSet3";
+            this.dataSet3.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // pOPULATIONBindingSource
+            // 
+            this.pOPULATIONBindingSource.DataMember = "POPULATION";
+            this.pOPULATIONBindingSource.DataSource = this.dataSet3;
+            // 
+            // pOPULATIONTableAdapter
+            // 
+            this.pOPULATIONTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.OPERATIONTableAdapter = null;
+            this.tableAdapterManager.POPULATIONTableAdapter = this.pOPULATIONTableAdapter;
+            this.tableAdapterManager.SUMMERTableAdapter = null;
+            this.tableAdapterManager.UpdateOrder = Olimpic_Sport.DataSet3TableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            this.tableAdapterManager.WINTERTableAdapter = null;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(495, 6);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(648, 558);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1157, 599);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.btn_reset);
+            this.tabPage1.Controls.Add(this.btn_delete);
+            this.tabPage1.Controls.Add(this.btn_update);
+            this.tabPage1.Controls.Add(this.btn_add);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Controls.Add(this.gb_population);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1149, 570);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(192, 71);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(92, 28);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label1.Size = new System.Drawing.Size(243, 56);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Countries";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(16, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Country";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(47, 179);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 23);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Code";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(16, 133);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 23);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Population";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(16, 181);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(164, 23);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "GDP per capita";
+            // 
+            // tb_country
+            // 
+            this.tb_country.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tb_country.Location = new System.Drawing.Point(217, 45);
+            this.tb_country.Name = "tb_country";
+            this.tb_country.Size = new System.Drawing.Size(100, 23);
+            this.tb_country.TabIndex = 6;
+            // 
+            // tb_code
+            // 
+            this.tb_code.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tb_code.Location = new System.Drawing.Point(217, 95);
+            this.tb_code.Name = "tb_code";
+            this.tb_code.Size = new System.Drawing.Size(100, 23);
+            this.tb_code.TabIndex = 7;
+            // 
+            // tb_population
+            // 
+            this.tb_population.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tb_population.Location = new System.Drawing.Point(217, 136);
+            this.tb_population.Name = "tb_population";
+            this.tb_population.Size = new System.Drawing.Size(100, 23);
+            this.tb_population.TabIndex = 8;
+            // 
+            // tb_gdp
+            // 
+            this.tb_gdp.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tb_gdp.Location = new System.Drawing.Point(217, 184);
+            this.tb_gdp.Name = "tb_gdp";
+            this.tb_gdp.Size = new System.Drawing.Size(100, 23);
+            this.tb_gdp.TabIndex = 9;
+            // 
+            // gb_population
+            // 
+            this.gb_population.Controls.Add(this.tb_gdp);
+            this.gb_population.Controls.Add(this.label2);
+            this.gb_population.Controls.Add(this.label4);
+            this.gb_population.Controls.Add(this.tb_population);
+            this.gb_population.Controls.Add(this.label5);
+            this.gb_population.Controls.Add(this.tb_code);
+            this.gb_population.Controls.Add(this.tb_country);
+            this.gb_population.Location = new System.Drawing.Point(31, 87);
+            this.gb_population.Name = "gb_population";
+            this.gb_population.Size = new System.Drawing.Size(392, 258);
+            this.gb_population.TabIndex = 10;
+            this.gb_population.TabStop = false;
+            this.gb_population.Text = "INFO";
+            // 
+            // btn_add
+            // 
+            this.btn_add.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_add.Location = new System.Drawing.Point(47, 393);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(87, 44);
+            this.btn_add.TabIndex = 11;
+            this.btn_add.Text = "Add";
+            this.btn_add.UseVisualStyleBackColor = true;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
+            // 
+            // btn_update
+            // 
+            this.btn_update.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_update.Location = new System.Drawing.Point(178, 394);
+            this.btn_update.Name = "btn_update";
+            this.btn_update.Size = new System.Drawing.Size(87, 43);
+            this.btn_update.TabIndex = 12;
+            this.btn_update.Text = "Update";
+            this.btn_update.UseVisualStyleBackColor = true;
+            this.btn_update.Click += new System.EventHandler(this.btn_update_Click);
+            // 
+            // btn_delete
+            // 
+            this.btn_delete.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_delete.Location = new System.Drawing.Point(313, 394);
+            this.btn_delete.Name = "btn_delete";
+            this.btn_delete.Size = new System.Drawing.Size(87, 43);
+            this.btn_delete.TabIndex = 13;
+            this.btn_delete.Text = "Delete";
+            this.btn_delete.UseVisualStyleBackColor = true;
+            this.btn_delete.Click += new System.EventHandler(this.btn_delete_Click);
+            // 
+            // btn_reset
+            // 
+            this.btn_reset.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_reset.Location = new System.Drawing.Point(178, 466);
+            this.btn_reset.Name = "btn_reset";
+            this.btn_reset.Size = new System.Drawing.Size(87, 43);
+            this.btn_reset.TabIndex = 14;
+            this.btn_reset.Text = "Reset";
+            this.btn_reset.UseVisualStyleBackColor = true;
+            this.btn_reset.Click += new System.EventHandler(this.btn_reset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1159, 602);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pOPULATIONBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.gb_population.ResumeLayout(false);
+            this.gb_population.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DataSet3 dataSet3;
+        private System.Windows.Forms.BindingSource pOPULATIONBindingSource;
+        private DataSet3TableAdapters.POPULATIONTableAdapter pOPULATIONTableAdapter;
+        private DataSet3TableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox tb_gdp;
+        private System.Windows.Forms.TextBox tb_population;
+        private System.Windows.Forms.TextBox tb_code;
+        private System.Windows.Forms.TextBox tb_country;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_reset;
+        private System.Windows.Forms.Button btn_delete;
+        private System.Windows.Forms.Button btn_update;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.GroupBox gb_population;
+    }
+}
+
---
+++ Olympic Sports/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.ManagedDataAccess.Client;
+using Oracle.ManagedDataAccess.Types;
+using System.Configuration;
+
+
+namespace Olimpic_Sport
+{
+    public partial class Form1 : Form
+    {
+        OracleConnection con = null;
+        public Form1()
+        {
+            this.setConnection();
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.updateDataGrid();
+        }
+        private void updateDataGrid()
+        {
+            OracleCommand cmd = con.CreateCommand();
+            cmd.CommandText = "SELECT * FROM population";
+            cmd.CommandType = CommandType.Text;
+            OracleDataReader dr = cmd.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Load(dr);
+            dataGridView1.DataSource = dt.DefaultView;
+            dr.Close();
+        }
+        private void setConnection()
+        {
+            String connectionString = ConfigurationManager.ConnectionStrings["myConnectionString"].ConnectionString;
+            con = new OracleConnection(connectionString);
+            try
+            {
+                con.Open();
+            }
+            catch (Exception exp) { }
+        }
+
+        private void AUD(string sql_stml, int state) {
+            String msg = "";
+            OracleCommand cmd = con.CreateCommand();
+            cmd.CommandText = sql_stml;
+            cmd.CommandType = CommandType.Text;
+
+            switch (state) {
+                case 0:
+                    msg = "Row Inserted Successfully";
+                    cmd.Parameters.Add("COUNTRY", OracleDbType.Varchar2, 20).Value = tb_country.Text;
+                    cmd.Parameters.Add("CODE", OracleDbType.Varchar2, 5).Value = tb_code.Text;
+                    cmd.Parameters.Add("POPULATION", OracleDbType.Int32, 20).Value = Int32.Parse(tb_population.Text);
+                    cmd.Parameters.Add("GDP_PER_CAPITA", OracleDbType.Int32, 20).Value = Int32.Parse(tb_country.Text);
+                    break;
+                case 1:
+                    msg = "Row Updated Successfully";
+                    cmd.Parameters.Add("CODE", OracleDbType.Varchar2, 5).Value = tb_code.Text;
+                    cmd.Parameters.Add("POPULATION", OracleDbType.Int32, 20).Value = Int32.Parse(tb_population.Text);
+                    cmd.Parameters.Add("GDP_PER_CAPITA", OracleDbType.Int32, 20).Value = Int32.Parse(tb_country.Text);
+                    cmd.Parameters.Add("COUNTRY", OracleDbType.Varchar2, 20).Value = tb_country.Text;
+                    break;
+                case 2:
+                    msg = "Row Deleted Successfully";
+                    cmd.Parameters.Add("COUNTRY", OracleDbType.Varchar2, 20).Value = tb_country.Text;
+                    break;
+            }
+            try {
+                int n = cmd.ExecuteNonQuery();
+                if (n > 0)
+                {
+                    MessageBox.Show(msg);
+                    this.updateDataGrid();
+                }
+            }
+            catch (Exception ex) { }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void resetAll()
+        {
+            tb_country.Text = "";
+            tb_code.Text = "";
+            tb_population.Text = "";
+            tb_gdp.Text = "";
+            
+            btn_add.Enabled = true;
+            btn_update.Enabled = false;
+            btn_delete.Enabled = false;
+        }
+
+        private void btn_reset_Click(object sender, EventArgs e)
+        {
+            this.resetAll();
+        }
+
+        private void btn_delete_Click(object sender, EventArgs e)
+        {
+            String sql = "DELETE FROM POPULATION " +
+                "WHERE COUNTRY = :COUNTRY";
+            this.AUD(sql, 2);
+            this.resetAll();
+        }
+
+        private void btn_update_Click(object sender, EventArgs e)
+        {
+            String sql = "UPDATE POPULATION SET CODE = :CODE," +
+               "POPULATION=:POPULATION, GDP_PER_CAPITA=:GDP_PER_CAPITA " +
+               "WHERE COUNTRY = :COUNTRY";
+            this.AUD(sql, 1);
+        }
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            String sql = "INSERT INTO POPULATION(COUNTRY, CODE, POPULATION, GDP_PER_CAPITA) " +
+               "VALUES(:COUNTRY, :CODE, :POPULATION, :GDP_PER_CAPITA)";
+            this.AUD(sql, 0);
+            btn_add.Enabled = false;
+            btn_update.Enabled = true;
+            btn_delete.Enabled = true;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGrid dg = sender as DataGrid;
+            DataRowView dr = (DataRowView)dg.SelectedItem as DataRowView;
+            if (dr != null)
+            {
+                tb_country.Text = dr["COUNTY"].ToString();
+                tb_code.Text = dr["CODE"].ToString();
+                tb_population.Text = dr["POPULATION"].ToString();
+                tb_gdp.Text = dr["GDP_PER_CAPITA"].ToString();
+
+                btn_add.Enabled = false;
+                btn_update.Enabled = true;
+                btn_delete.Enabled = true;
+
+            }
+        }
+    }
+}
